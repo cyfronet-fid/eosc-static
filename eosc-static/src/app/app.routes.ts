6d06e08f61e18408105ffa7c7196132f2d02b55c
mich.kolomanski <mich.kolomanski@gmail.com>
2024-04-12 18:04:13 +0200

    fix: styles

--- eosc-static/src/app/app.routes.ts
+++ eosc-static/src/app/app.routes.ts
@@ -1,8 +1,7 @@
 import { Routes } from '@angular/router';
-import { EoscClosureComponent } from './eosc-closure/eosc-closure.component';
+import {AppComponent} from "./app.component";
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/eosc-message' },
-  { path: 'eosc-message', component: EoscClosureComponent },
-  { path: '**', redirectTo: '/eosc-message' }
+  { path: '', component: AppComponent},
+  { path: '**', redirectTo: '' }
 ];
